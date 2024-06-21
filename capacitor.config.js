8b13efbe24be90b40c1ca21fed16fdfd6068ef1e
DanielTrujilloTapia <22300119@uttt.edu.mx>
2024-06-21 12:55:40 -0600

    first commit

---
+++ capacitor.config.js
@@ -0,0 +1,7 @@
+const config = {
+  appId: 'io.ionic.sitem',
+  appName: 'Sitem',
+  webDir: 'dist'
+};
+
+module.exports = config;
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        Home componente
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'HomeComponent',
+}
+</script>
