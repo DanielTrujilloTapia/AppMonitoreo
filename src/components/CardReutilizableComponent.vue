ab8fc230494f48e6a98c8ff26bd720ada08e9136
DanielTrujilloTapia <22300119@uttt.edu.mx>
2024-06-24 00:03:38 -0600

    Add: HomeComponent and CardReutilizableComponent

---
+++ src/components/CardReutilizableComponent.vue
@@ -0,0 +1,71 @@
+<template>
+    <ion-card style="margin: 0; margin-bottom: 5px; margin-top: 5px;">
+        <ion-grid>
+            <ion-row>
+                <ion-col size="4" class="container-image">
+                    <ion-img :src="img" class="custom-image"></ion-img>
+                </ion-col>
+                <ion-col size="8" class="container-text">
+                    <div>
+                        <p>{{ title }}</p>
+                        <p>{{ subtitle }}</p>
+                        <p v-if=" textOne != null" ><ion-icon :icon="iconOne" color="success" ></ion-icon> {{textOne}}</p>
+                        <p v-if=" textTwo != null" ><ion-icon :icon="iconTwo" color="success" ></ion-icon> {{textTwo}}</p>
+                    </div>
+                </ion-col>
+            </ion-row>
+        </ion-grid>
+    </ion-card>
+</template>
+
+<script>
+import { IonCard, IonGrid, IonCol, IonRow, IonImg, IonIcon } from '@ionic/vue'
+
+export default {
+    name: 'CardReutilizableComponent',
+    components: {
+        IonCard,
+        IonGrid,
+        IonCol,
+        IonRow,
+        IonImg,
+        IonIcon
+    },
+    props: {
+        img: String,
+        iconOne: String,
+        iconTwo: String,
+        title: String,
+        subtitle: String,
+        textOne: String,
+        textTwo: String,
+    }
+}
+</script>
+
+<style>
+.container-image{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    border-right: 1px solid;
+    border-image: linear-gradient(transparent 1%, white 5%, rgb(100, 100, 100) 50%, white 95%, transparent 100%) 1; 
+}
+
+.container-text{
+    display: flex;
+    align-items: center;
+    padding-left: 15px;
+}
+
+.custom-image {
+    width: 100px;
+    height: 100px;
+}
+
+ion.icon{
+    margin-right: 5px; 
+}
+
+</style>
--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,12 +1,36 @@
 <template>
-    <div>
-        Home componente
-    </div>
+    <ion-page>
+        <ion-content>
+            <card-reutilizable-component :img="'planificacion-de-tareas.png'" :iconOne="addCircleOutline" :title="'Servicios SITEM'" :subtitle="'Tareas generadas con servicios'" :textOne="'Nueva Tarea'" :iconTwo="eyeOutline" :textTwo="'Consultar Tareas'" />
+            <card-reutilizable-component :img="'falla2.png'" :title="'Fallas Mecanicas'" :subtitle="'Tareas sobre fallos mecanicos'" :iconOne="addCircleOutline" :textOne="'Crear Tarea'" :iconTwo="eyeOutline" :textTwo="'Consultar Tareas'" />
+            <card-reutilizable-component :img="'calendario.png'" :title="'Tareas Automatizadas'" :subtitle="'Tareas automaticas generadas mensualmente'" :iconOne="addCircleOutline" :textOne="'Nueva Tarea'" :iconTwo="eyeOutline" :textTwo="'Consultar Tareas'" />
+            <card-reutilizable-component :img="'vigilancia.png'" :title="'Monitoreo de Tareas'" :subtitle="'Control y supervision de tareas'" :iconOne="eyeOutline" :textOne="'Monitorear tareas'" />
+            <card-reutilizable-component :img="'usuario.png'" :title="'Usuarios SITEM'" :subtitle="'Gestionar usuarios existentes'" :iconOne="addCircleOutline" :textOne="'Crear nuevo usuario'" :iconTwo="eyeOutline" :textTwo="'Consultar usuarios'" />
+        </ion-content>
+    </ion-page>
 </template>
 
 <script>
+import { IonPage, IonContent } from '@ionic/vue';
+import CardReutilizableComponent from '../components/CardReutilizableComponent.vue'
+import { addCircleOutline, eyeOutline } from 'ionicons/icons';
 
 export default {
     name: 'HomeComponent',
+    components: {
+        IonPage,
+        IonContent,
+        CardReutilizableComponent,
+        
+    },
+    setup() {
+        return {
+            addCircleOutline,
+            eyeOutline
+        }
+    }
 }
 </script>
+
+<style>
+</style>
