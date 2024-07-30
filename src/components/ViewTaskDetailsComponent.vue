19b3d6f50bf3c223152590202754fc1fd3c19edc
DanielTrujilloTapia <22300119@uttt.edu.mx>
2024-07-29 22:26:56 -0600

    Update: style

--- src/components/ViewTaskDetailsComponent.vue
+++ src/components/ViewTaskDetailsComponent.vue
@@ -1,51 +1,84 @@
 <template>
     <ion-page>
-        <ion-card>
-            <ion-header>
-                <p @click.prevent="navigateBack">volver</p>
-            </ion-header>
-            {{ tarea.nom_tarea_servicio }}
-            <br>
-            <br>
-            {{ tarea.fecha_publicacion_servicio }}
-            <br>
-            <br>
-            {{ tarea.fecha_entega_servicio }}
-            <br>
-            <br>
-            {{ tarea?.service?.nom_servicio || 'Sin servicio asignado' }}
-            <br>
-            <br>
-            {{ tarea?.plants?.nom_planta || 'Sin servicio asignado' }}
-            {{ tarea?.plants?.descripcion_planta || 'Sin servicio asignado' }}
-            <br>
-            <br>
-            {{ tarea?.userEncargado?.nom_usuario || 'Sin servicio asignado' }}
-            <br>
-            <br>
-            {{ tarea?.userAyudante?.nom_usuario || 'Sin servicio asignado' }}
-            <br>
-            <br>
-            {{ tarea?.userAdmin?.nom_usuario || 'Sin servicio asignado' }}
-            <br>
-            <br>
-            {{ tarea?.status?.nom_estatus || 'Sin servicio asignado' }}
-            <br>
-            <br>
-            {{ tarea?.priority?.nom_prioridad || 'Sin servicio asignado' }}
-        </ion-card>
+        <ion-content>
+            <ion-card style="margin: 0px;">
+                <ion-card-header>
+
+                    <ion-row>
+                        <ion-col style="display: flex; justify-content: end;">
+                            <p @click.prevent="navigateBack" style="cursor: pointer;">Cerrar</p>
+                        </ion-col>
+                    </ion-row>
+                    
+                    <ion-row>
+                        <ion-col>
+                            <p class="title-size">{{ tarea.nom_tarea_servicio }}</p>
+                        </ion-col>
+                    </ion-row>
+                    
+                    <ion-row>
+                        <ion-col>
+                            <p class="subtitle-size">La tarea se entrega: {{ formatDate(tarea.fecha_entega_servicio) }}</p>
+                        </ion-col>
+                    </ion-row>
+
+                </ion-card-header>
+    
+                <ion-card-content>
+                    <ion-item lines="none">
+                        <ion-label class="subtitle-size" position="stacked">Planta</ion-label>
+                        <p class="text-size">{{ tarea?.plants?.nom_planta || 'Sin planta asignada' }}</p>
+                    </ion-item>
+                    <ion-item lines="none">
+                        <ion-label class="subtitle-size" position="stacked">Servicio</ion-label>
+                        <p class="text-size" >{{ tarea?.service?.nom_servicio || 'Sin servicio asignado' }}</p>
+                    </ion-item>
+                    <ion-item lines="none">
+                        <ion-label class="subtitle-size" position="stacked">Encargado</ion-label>
+                        <p class="text-size" >{{ tarea?.userEncargado?.nom_usuario || 'Sin encargado asignado' }}</p>
+                    </ion-item>
+                    <ion-item lines="none">
+                        <ion-label class="subtitle-size" position="stacked">Ayudante</ion-label>
+                        <p class="text-size" >{{ tarea?.userAyudante?.nom_usuario || 'Sin ayudante asignado' }}</p>
+                    </ion-item>
+                    <ion-item lines="none">
+                        <ion-label class="subtitle-size" position="stacked">Estatus</ion-label>
+                        <p class="text-size" >{{ tarea?.status?.nom_estatus || 'Sin estatus asignado' }}</p>
+                    </ion-item>
+
+                    <ion-row>
+                        <ion-col sise="10" style="margin: 0px; padding: 0px">
+                            <ion-item lines="none">
+                                <ion-label class="subtitle-size" position="stacked">Prioridad</ion-label>
+                                <p class="text-size" >{{ tarea?.priority?.nom_prioridad || 'Sin prioridad asignada' }}</p>
+                            </ion-item>
+                        </ion-col>
+                        <ion-col size="4" style="display: flex; justify-content: center; align-items: center; padding-top: 20px;">
+                            <ion-button>Iniciar</ion-button>
+                        </ion-col>
+                    </ion-row>
+                </ion-card-content>
+            </ion-card>
+        </ion-content>
     </ion-page>
 </template>
 
 <script>
-import { IonPage } from '@ionic/vue';
+import { IonPage, IonContent, IonCard, IonCardContent, IonCardHeader, IonRow, IonCol, IonItem, IonLabel } from '@ionic/vue';
 import { useIonRouter } from '@ionic/vue';
-
 
 export default {
     name: 'ViewTaskDetailsComponent',
     components: {
-        IonPage
+        IonPage,
+        IonContent,
+        IonCard,
+        IonCardContent,
+        IonCardHeader,
+        IonRow,
+        IonCol,
+        IonItem,
+        IonLabel
     },
     data() {
         return {
@@ -106,10 +139,62 @@ export default {
             } catch (error) {
                 console.log('Sucedió un error:', error);
             }
-        }
+        },
+        
+        formatDate(dateString) {
+            const date = new Date(dateString);
+            const day = date.getDate();
+            const month = date.toLocaleString('default', { month: 'short' });
+            const year = date.getFullYear();
+            return `${day} de ${month} de ${year}`;
+        },
     },
     created() {
         this.GetDetailsTask();
     }
 }
 </script>
+
+
+<style scoped>
+
+.title-size{
+    font-size: 15px;
+    color: var(--ion-title-color);
+    font-weight: bold;
+    text-align: justify;
+}
+
+.subtitle-size{
+    font-size: 14px;
+    color: var(--ion-subtitle-color);
+    text-align: justify;
+}
+
+.text-size{
+    font-size: 13px;
+    text-align: justify;
+}
+
+p {
+    margin: 0;
+    padding: 0;
+}
+
+ion-item {
+    --padding-start: 0px;
+    --inner-padding-end: 0px;
+    --inner-padding-start: 0px;
+    --padding-end: 0px;
+    margin: 10px 0;
+}
+
+ion-label {
+    font-weight: bold;
+}
+
+p {
+    margin-top: 5px;
+    margin-bottom: 5px;
+}
+</style>
